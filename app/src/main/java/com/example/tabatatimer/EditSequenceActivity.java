5807a5b627783e90cadfeb7ba5f2c9e6c65d826a
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-09-21 17:20:21 +0300

    New Edit sequence activity.

--- app/src/main/java/com/example/tabatatimer/EditSequenceActivity.java
+++ app/src/main/java/com/example/tabatatimer/EditSequenceActivity.java
@@ -1,13 +1,73 @@
 package com.example.tabatatimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class EditSequenceActivity extends AppCompatActivity {
+    ArrayList<SequenceControllerModel> items_order;
+    private WorkoutModel workout;
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_sequence);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        dbHelper = new DatabaseHelper(this);
+        ListView listView = (ListView) findViewById(R.id.editSequenceListView);
+        workout = (WorkoutModel) getIntent().getSerializableExtra("SEQUENCE");
+        EditText editText = (EditText) findViewById(R.id.editPageTextView);
+        editText.setText(workout.getWorkout_name());
+
+        items_order = new ArrayList<SequenceControllerModel>();
+
+        ArrayList<String> fazes = workout.getWorkout_order();
+        ArrayList<Integer> fazes_time = workout.getWorkout_order_time();
+        for(int i = 0; i < fazes.size(); i++) {
+            int imageId = 0;
+            String faze = fazes.get(i);
+            switch (faze) {
+                case "Work": imageId = R.drawable.access_alarm; break;
+                case "Rest": imageId = R.drawable.accessibility; break;
+                case "Preparation": imageId = R.drawable.directions_walk; break;
+                case "Stretch": imageId = R.drawable.directions_run; break;
+                case "SetsRest": imageId = R.drawable.weekend; break;
+                default: break;
+            }
+
+            items_order.add(new SequenceControllerModel(imageId, faze, fazes_time.get(i)));
+        }
+        listView.setAdapter(new SequenceAdapter(items_order, workout, this, ""));
+    }
+
+    public void saveChangesButton(View view) {
+        ArrayList<String> fazes = workout.getWorkout_order();
+        ArrayList<Integer> fazes_time = workout.getWorkout_order_time();
+        EditText editText = (EditText)findViewById(R.id.editPageTextView);
+        workout.setWorkout_name(editText.getText().toString());
+        workout.updateDescription();
+
+        for (int i = 0; i < fazes.size(); i++) {
+            fazes.set(i, items_order.get(i).getTitle());
+            fazes_time.set(i, items_order.get(i).getBaseValue());
+        }
+
+        if (workout.getId() != 0)
+            workout.updateWorkout(dbHelper);
+        else
+            workout.saveWorkout(dbHelper);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
