3138a99b175fa0287f9aafd25969e1e0702c1797
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-09-21 17:19:11 +0300

    Added resource files. Update.

---
+++ app/src/main/java/com/example/tabatatimer/EditSequenceActivity.java
@@ -0,0 +1,13 @@
+package com.example.tabatatimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditSequenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_sequence);
+    }
+}
