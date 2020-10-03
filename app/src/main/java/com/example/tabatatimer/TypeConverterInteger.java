ca78b6288fdc47b1e47a126cdd91da361639c52b
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-03 23:12:39 +0300

    Converter. Convert List<Integer> to json format.

---
+++ app/src/main/java/com/example/tabatatimer/TypeConverterInteger.java
@@ -0,0 +1,22 @@
+package com.example.tabatatimer;
+
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeConverterInteger {
+
+    @TypeConverter
+    public static ArrayList<Integer> restoreList(String listOfIntegers) {
+        return new Gson().fromJson(listOfIntegers, new TypeToken<ArrayList<Integer>>() {}.getType());
+    }
+
+    @TypeConverter
+    public static String saveList(ArrayList<Integer> listOfIntegers) {
+        return new Gson().toJson(listOfIntegers);
+    }
+}
