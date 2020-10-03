13bf1a14c2767af04aed6fb346b913937c89c301
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-03 23:13:52 +0300

    Added database application objects. Simpy - handler of SQL commands.

---
+++ app/src/main/java/com/example/tabatatimer/WorkoutDao.java
@@ -0,0 +1,40 @@
+package com.example.tabatatimer;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
+public interface WorkoutDao {
+    @Query("SELECT * FROM workouts")
+    List<WorkoutModel> getAll();
+
+    @Query("SELECT * FROM workouts WHERE id = :id")
+    WorkoutModel getById(long id);
+
+    @Query("SELECT * FROM workouts WHERE workout_name = :name")
+    WorkoutModel getByName(String name);
+
+    @Query("SELECT * FROM workouts WHERE id IN (:userIds)")
+    List<WorkoutModel> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM workouts WHERE workout_name LIKE :name")
+    WorkoutModel findByName(String name);
+
+    @Query("SELECT COUNT(id) FROM workouts")
+    int getCount();
+
+    @Insert
+    void insert(WorkoutModel workout);
+
+    @Update
+    void update(WorkoutModel workout);
+
+    @Delete
+    void delete(WorkoutModel workout);
+}
